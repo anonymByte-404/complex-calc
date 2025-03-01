b39179b96b0bbea21ec3081452bebc55426fc0ea
Aeron Asug <an0nymnx@outlook.com>
2025-03-01 03:35:30 +0000

    chore: update header to include necessary libraries

--- src/algebra_solver.h
+++ src/algebra_solver.h
@@ -1,10 +1,14 @@
-#ifndef ALGEBRA_SOLVER_H
-#define ALGEBRA_SOLVER_H
+#ifndef ALGEBRA_SOLVER_H_
+#define ALGEBRA_SOLVER_H_
 
 #include <vector>
+#include <cmath>
+#include <iomanip>
 
 void solveLinearEquation(double a, double b);
+
 void solveQuadraticEquation(double a, double b, double c);
-double evaluatePolynomial(const std::vector<double> &coefficients, double x);
+
+double evaluatePolynomial(const std::vector<double>& coefficients, double x);
 
 #endif
