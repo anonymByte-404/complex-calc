881a49ee6efefa393f0c34909e255ce7874f4cf3
Aeron Asug <an0nymnx@outlook.com>
2025-03-01 23:53:59 +0000

    feat: Create main.h to centralize function and matrix operation headers

---
+++ include/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// Functions
+#include "./logic/functions/cubic.h"
+#include "./logic/functions/linear.h"
+#include "./logic/functions/octic.h"
+#include "./logic/functions/quadratic.h"
+#include "./logic/functions/quartic.h"
+#include "./logic/functions/quintic.h"
+#include "./logic/functions/septic.h"
+#include "./logic/functions/sextic.h"
+
+// Matricies
+#include "./logic/matrices/matrix_addition.h"
+#include "./logic/matrices/matrix_subtraction.h"
+#include "./logic/matrices/matrix_utils.h"
+
+#endif
